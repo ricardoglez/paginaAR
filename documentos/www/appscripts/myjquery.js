4638beffec2e52bf124a05266571fa46d754d942
ricardoglez <ricardogonzalezamora@gmail.com>
2016-09-21 00:22:01 -0500

    Avances en el layout general de la pagina

--- documentos/www/appscripts/myjquery.js
+++ documentos/www/appscripts/myjquery.js
@@ -1,1 +1,28 @@
+$(document).ready(function() {
 
+    var stickyNavTop = $('#bread').offset().top;
+    stickyNavTop = stickyNavTop - 90;
+
+    $('#bread').wrap('<div class="bread-placeholder"></div>');
+
+    $('.bread-placeholder').height($('#bread').outerHeight());
+
+    $('#bread').wrapInner('<div class= "bread-inner"><div/>');
+    var stickyNav = function() {
+
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop >= stickyNavTop) {
+            $('#bread').addClass('sticky');
+            $('.bread-placeholder').addClass('sticky');
+        } else {
+            $('#bread').removeClass('sticky');
+            $('.bread-placeholder').removeClass('sticky');
+        }
+    };
+
+    stickyNav();
+
+    $(window).scroll(function() {
+        stickyNav();
+    });
+});
